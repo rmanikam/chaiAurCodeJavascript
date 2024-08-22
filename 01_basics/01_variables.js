cb707ea2f30150f2a09fe1983cfca3deada873b1
Raman Sahi <sahiraman7@gmail.com>
2024-08-22 21:15:33 +0530

    javascript chai aur code series

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+
+accountCity = "Jaipur";
+
+let accountState;
+// accountId = 2; // getting error assignment to constant variable
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bangalore";
+console.log(accountId);
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+/*
+  
+Prefer not to use var because of issue 
+in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,44 @@
+// "use strict"; // treat all JS Code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // dont do this in console.
+//  code readibility should be high
+
+console.log("Hitesh");
+
+let name = "hitesh"; // string data type
+
+let age = 18; // data type is number
+
+let isLoggedIn = false; // data type is boolean
+
+// number => 2 to power 53  // range of number is 2 to power 53
+
+//bigint // if number is too large use bigint
+
+//string => "" always use string in double quotes
+
+// boolean => true/false
+
+// null => standalone value.
+
+// null is representation of empty value.
+// empty doesnt mean like it is a empty string like this
+// state ="". null is a special type.
+// let state = null. null is not undefined but is khali
+
+// undefined =>  i have declared a variable but has
+// not been assigned it a value
+
+// symbol => unique
+
+// object
+
+console.log(typeof "hitesh"); // ans is string
+
+console.log(typeof age); // ans is number
+
+console.log(typeof null); // ans is object
+
+console.log(typeof undefined); // ans is undefined
